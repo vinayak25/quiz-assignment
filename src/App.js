66f4ea51adffcdbbf0395faadabbbd58800641e1
Vinayak Sarawagi <vinayak.sarawagi@squareboat.com>
2023-12-12 12:38:27 +0530

    FIX: bugs

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { getQuizzes } from './data/quizzes';
-import { formatQuestions } from './helpers';
-
-import { QuizContainer } from './QuizContainer';
-
+import React from 'react';
 import './styles.css';
 
 const App = () => {
-
   return (
     <div className="app">
-    <div>
-      <h1>Hey there!</h1>
-      <h2>Let's build something cool together</h2>
-    </div>
+      <div>
+        <h1>Hey there!</h1>
+        <h2>Let's build something cool together</h2>
+      </div>
     </div>
   );
 };
